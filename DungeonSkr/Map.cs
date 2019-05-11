7061bc259e95e0a7f3a935908bd5d3c4494a8178
Mr Sloth <leonpfeil@gmail.com>
2019-05-11 23:32:54 +0200

    basics

---
+++ DungeonSkr/Map.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSkr
+{
+    enum cellType { friendly, neutral, hostile }; 
+    
+    class Map
+    {
+        //make it load 
+        Cell[,] map = new Cell[1, 2];
+
+        public void loadMap()
+        {
+            
+        }
+    }
+
+    class Cell
+    {
+        string name;
+        string description;
+        public cellType typ;
+        NPC npc;
+        //possible ways
+        bool left;
+        bool right;
+        bool up;
+        bool down;
+
+        
+    }
+}
---
+++ DungeonSkr/NPC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSkr
+{
+    abstract class NPC
+    {
+        string name;
+
+    }
+
+    abstract class Enemy : NPC
+    {
+        int health;
+        int level;
+        Weapons weapon;
+        Wearables armor;
+
+
+        //TODO: stats
+        
+    }
+
+
+}
